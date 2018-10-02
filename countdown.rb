238a35cf8b782bfe4416bbb1b2da5654bbe93152
Suheyl Turac <skbsturac@live.com>
2018-10-02 22:25:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  i = 10
+    while i > 0
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep 5
 end
